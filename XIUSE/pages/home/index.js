9677aac31e1c45ee58ccfad865cf30f4497dae9b
zhuaofei <2353536563@qq.com>
2018-08-14 23:39:43 +0800

    add files

---
+++ XIUSE/pages/home/index.js
@@ -0,0 +1,104 @@
+// pages/home/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banner: './images/Rectangle4@3x.png',
+    itemList: [
+      {
+        name: '我要加工',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '我要安装',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '购买辅料',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '加工方案',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '优惠券',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '我要验收',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '整体方案',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '进入嗅色',
+        path: './images/加工@2x.png'
+      },
+      {
+        name: '服务商',
+        path: './images/加工@2x.png'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ XIUSE/pages/login/index.js
@@ -0,0 +1,94 @@
+// pages/login/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    username: '',
+    password: '',
+    aiUrl: './images/Group3@2x.png',
+    user: './images/user@3x.png',
+    pas: './images/lock-ico@3x.png'
+  },
+  //用户名
+  userName: function (e) {
+    this.setData({
+      username: e.detail.value
+    })
+  },
+  //密码
+  userPass: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+  register() {
+    wx.navigateTo({
+      url: "../register/index"
+    })
+  },
+  login() {
+    // wx.redirectTo({
+    //   url: "../home/index"
+    // })
+    wx.switchTab({
+      url: '../home/index'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
