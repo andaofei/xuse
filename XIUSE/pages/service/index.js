e7437312edb357eea6f67951559d6dfaf9d91e39
zhuaofei <2353536563@qq.com>
2018-08-17 00:54:26 +0800

    update

--- XIUSE/pages/service/index.js
+++ XIUSE/pages/service/index.js
@@ -5,7 +5,93 @@ Page({
    * 页面的初始数据
    */
   data: {
-      avator: './images/user.png'
+      avator: './images/user.png',
+      orderList: [
+        {
+          name: '待报价',
+          id: 1,
+          src: './images/待报价@2x.png'
+        },
+        {
+          name: '待接收',
+          id: 2,
+          src: './images/待接收@2x.png'
+        }, 
+        {
+          name: '待加工',
+          id: 3,
+          src: './images/待加工@2x.png'
+        },
+        {
+          name: '接收材料',
+          id: 4,
+          src: './images/接收材料@2x.png'
+        },
+        {
+          name: '正在加工',
+          id: 5,
+          src: './images/正在加工@2x.png'
+        },
+        {
+          name: '加工完成',
+          id: 6,
+          src: './images/加工完成@2x.png'
+        },
+        {
+          name: '待验收',
+          id: 7,
+          src: './images/待验收@2x.png'
+        },
+        {
+          name: '待发货',
+          id: 8,
+          src: './images/待发货@2x.png'
+        },
+        {
+          name: '确认发货',
+          id: 9,
+          src: './images/确认发货@2x.png'
+        },
+        {
+          name: '更多选项',
+          id: 10,
+          src: './images/更多@2x.png'
+        }
+      ],
+      Installation: [
+        {
+          name: '待报价',
+          id: 1,
+          src: './images/报价@2x.png'
+        },
+        {
+          name: '待安装',
+          id: 2,
+          src: './images/待安装@2x.png'
+        },
+        {
+          name: '已取消',
+          id: 3,
+          src: './images/取消@2x.png'
+        },
+        {
+          name: '已完成',
+          id: 4,
+          src: './images/完成@2x.png'
+        }
+      ],
+      management: [
+          {
+            name: '图纸管理',
+            id: 1,
+            src: './images/图纸审查@2x.png'
+          },
+          {
+            name: '工单处理',
+            id: 2,
+            src: './images/工单处理@2x.png'
+          }
+      ]
   },
 
   /**
